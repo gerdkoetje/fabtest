29b242992ca6114c13799e587aaeb9dd49cf7ef9
gerd <gerd@nbhosting.nl>
2013-11-03 01:28:21 +0100

    test push

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fabtest',
+    version='1',
+    packages=['fabtest', 'fabtesting'],
+    url='',
+    license='',
+    author='NBhosting',
+    author_email='',
+    description=''
+)
